14ee38269f51fbc1efbe99be54c7d48afa72fb0a
trang <trang@lenovo-debian.192.168.1.1>
2025-11-20 01:36:02 +0700

    backup 20 nov 2025

---
+++ .config/emacs/elpa/prism-20241024.40/prism-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "prism" "20241024.40"
+  "Customizable, depth-based syntax coloring."
+  '((emacs  "27.1")
+    (compat "29.1.4.5")
+    (dash   "2.14.1"))
+  :url "https://github.com/alphapapa/prism.el"
+  :commit "2fa8eb5a9ca62a548d33befef4517e5d0266eb28"
+  :revdesc "2fa8eb5a9ca6"
+  :keywords '("faces" "lisp")
+  :authors '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers '(("Adam Porter" . "adam@alphapapa.net")))
