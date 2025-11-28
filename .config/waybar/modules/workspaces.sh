48e045c479139ea835a01cbdc2a946960a12ee0e
trang <trang@lenovo-debian.192.168.1.1>
2025-11-28 21:25:01 +0700

    niri workspace icon

--- .config/waybar/modules/workspaces.sh
+++ .config/waybar/modules/workspaces.sh
@@ -10,11 +10,20 @@ down)
 *)
     glyphs=""
     workspace_str=" " 
-    for ws in $(niri msg -j workspaces | jq ".[] | select(.output == \"$1\") | .is_active"); do
-        workspace_str="$workspace_str$( if "$ws"; then
+    
+    # Lấy cả danh sách workspace và windows để so khớp
+    # Logic lọc: Giữ lại workspace nếu (nó đang Active) HOẶC (ID của nó có xuất hiện trong danh sách windows)
+    data=$(jq -n --argjson ws "$(niri msg -j workspaces)" --argjson wins "$(niri msg -j windows)" \
+        '$ws[] | select(.output == "'"$1"'") | select(.is_active or (.id as $id | $wins | any(.workspace_id == $id))) | .is_active')
+
+    for ws in $data; do
+        workspace_str="$workspace_str$( if [ "$ws" = "true" ]; then
                  echo "<span color='#e8fc99'>${glyphs:0:1}</span>";
-            else echo "${glyphs:1:1}"; fi)  "
+            else 
+                 echo "${glyphs:1:1}"; fi)  "
     done
+    
+    # Lấy tên workspace đang active (để hiển thị tooltip)
     name=$(niri msg -j workspaces | jq -r ".[] | select(.output == \"$1\" and .is_active == true) | .name")
     echo -e "{\"text\":\"${workspace_str}\", \"tooltip\":\"Active workspace name: ${name}\"}"
 esac
