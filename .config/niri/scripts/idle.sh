ebf5aace068ce7c48b3975bcdab4c0fc549a9dc3
trang <trang@lenovo-debian.192.168.1.1>
2025-12-28 22:38:46 +0700

    backup 28 dec 25

--- .config/niri/scripts/idle.sh
+++ .config/niri/scripts/idle.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# 2-minute (120s) timeout to dim the screen
+# 2-minute (120s) timeout to dim the screen (Currently missing in your exec command)
 # 4-minute (240s) timeout to lock the screen
 # 5-minute (300s) timeout to turn off the monitors
 
 exec swayidle -w \
-    timeout 240 'gtklock' \
+    timeout 200 'brightnessctl -s & brightnessctl set 1000'\
+    resume 'brightnessctl -r' \
+    timeout 240 'gtklock -d' \
     timeout 300 'niri msg action power-off-monitors' \
     resume 'niri msg action power-on-monitors' \
-    before-sleep 'gtklock'
+    before-sleep 'gtklock -d'
